75f557e73453d2563b8dd9edd04977f2184f42e1
Cecil Phillip <cecilphillip@gmail.com>
2014-07-15 00:28:37 -0400

    added OAuth2 Resource Owner sample

---
+++ OAuthResourceOwnerServer/Startup.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Web.Http;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Newtonsoft.Json;
+using OAuthResourceOwnerServer;
+using OAuthResourceOwnerServer.Providers;
+using Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace OAuthResourceOwnerServer
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // token generation
+            app.UseOAuthAuthorizationServer(new OAuthAuthorizationServerOptions
+            {
+                // for demo purposes
+                AllowInsecureHttp = true,
+
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromHours(8),
+
+                Provider = new CustomAuthorizationServerProvider()
+            });
+
+            // token consumption
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+            ConfigureWebApi(app);
+        }
+
+        private void ConfigureWebApi(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "{controller}/{action}"
+            );
+
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Formatting.Indented;
+
+            app.Map("/api", api => api.UseWebApi(config));
+        }
+    }
+}
