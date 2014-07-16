3e9db0622ff08325b38fa6fb9bd30643565bd00a
Cecil Phillip <cecilphillip@gmail.com>
2014-07-16 16:44:48 -0400

    added client credential flow sample

--- OAuthResourceClient/Program.cs
+++ OAuthResourceClient/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net.Http;
+using System.Threading.Tasks;
 using Thinktecture.IdentityModel.Client;
 
 namespace OAuthResourceClient
@@ -8,19 +9,40 @@ namespace OAuthResourceClient
     {
         static void Main(string[] args)
         {
-            ResourceOwnerFlow();
+            var task = ClientCredentialFlow();
+
+            Task.WaitAll(task);
 
             Console.ReadLine();
         }
 
-        private static void ResourceOwnerFlow()
+        private async static Task ClientCredentialFlow()
+        {
+            try
+            {
+                var oauthClient = new OAuth2Client(new Uri("http://localhost:1642/token"), "angular", "secret");
+                var tokenResponse = oauthClient.RequestClientCredentialsAsync("read").Result;
+
+                var client = new HttpClient();
+                client.SetBearerToken(tokenResponse.AccessToken);
+
+                var response = await client.GetStringAsync(new Uri("http://localhost:1642/api/secure/data"));
+                Console.WriteLine(response);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private async static Task ResourceOwnerFlow()
         {
             var oauthClient = new OAuth2Client(new Uri("http://localhost:1642/token"));
             var tokenResponse = oauthClient.RequestResourceOwnerPasswordAsync("cecil", "cecil").Result;
-            
+
             var client = new HttpClient();
             client.SetBearerToken(tokenResponse.AccessToken);
-            var response = client.GetStringAsync(new Uri("http://localhost:1642/api/secure/data")).Result;
+            var response = await client.GetStringAsync(new Uri("http://localhost:1642/api/secure/data"));
 
             Console.WriteLine(response);
         }
--- OAuthResourceServer/Providers/CustomAuthorizationServerProvider.cs
+++ OAuthResourceServer/Providers/CustomAuthorizationServerProvider.cs
@@ -9,19 +9,46 @@ namespace OAuthResourceServer.Providers
         public override Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
         {
             // OAuth2 supports the notion of client authentication
-            // this is not used here
-            context.Validated();
-            return Task.FromResult<object>(null);
+            string clientId = string.Empty;
+            string clientSecret = string.Empty;
+
+            if(!context.TryGetBasicCredentials(out clientId, out clientSecret))
+            {
+                context.Validated();
+
+                return Task.FromResult(0);
+            }
+
+            //check your client info!!
+            if(clientId == "angular")
+            {
+                context.Validated();
+            }
+
+            return Task.FromResult(0);
         }
 
-        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        public override Task GrantClientCredentials(OAuthGrantClientCredentialsContext context)
+        {
+            if(context.ClientId != null)
+            {
+                var id = new ClaimsIdentity(context.Options.AuthenticationType);
+                id.AddClaim(new Claim("sub", context.ClientId));
+                id.AddClaim(new Claim("role", "user"));
+                context.Validated(id);
+            }
+
+            return Task.FromResult(0);
+        }
+
+        public override Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
         {
             // validate user credentials (demo!)
             // user credentials should be stored securely (salted, iterated, hashed yada)
-            if (context.UserName != context.Password)
+            if(context.UserName != context.Password)
             {
                 context.Rejected();
-                return;
+                return Task.FromResult(0);
             }
 
             // create identity
@@ -30,6 +57,13 @@ namespace OAuthResourceServer.Providers
             id.AddClaim(new Claim("role", "user"));
 
             context.Validated(id);
+            return Task.FromResult(0);
+        }
+
+        public override Task TokenEndpoint(OAuthTokenEndpointContext context)
+        {
+            //Called at the final stage of a successful Token endpoint request. do any final modification of the claims being used to issue access or refresh tokens
+            return base.TokenEndpoint(context);
         }
     }
 }
